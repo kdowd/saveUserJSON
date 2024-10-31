a7e6d74fee37c35a859003dc8113ef63c9daf26d
kdowd <kevin.dowd@gmail.com>
2024-11-01 12:19:12 +1300

    working code

--- saveUserJSON.cpp
+++ saveUserJSON.cpp
@@ -23,6 +23,29 @@ struct RelatedMovie {
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(RelatedMovie, name, year)
 
 int main() {
+	Person Director{ "George Lucas", 1944 };
+	vector<string> Cast{ "Mark Hamill", "Harrison Ford","Carrie Fisher" };
+	Cast.push_back("KJ Dowd");
+
+
+	vector<RelatedMovie> RelatedMovies{
+		{"Rogue One", 2016}, {"Dune", 2021} };
+
+	json UserDataAsJSON{
+		{"movie", "Star Wars"},
+		{"released", true},
+		{"year", 1977},
+		{"cast", Cast},
+		{"director", Director},
+		{"related_movies", RelatedMovies}
+	};
+
+	//std::cout << UserDataAsJSON.dump(4);
+
+	ofstream myfile;
+	myfile.open("example.txt");
+	myfile << UserDataAsJSON;
+	myfile.close();
 
 	system("pause");
 	return 0;
